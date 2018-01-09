cf74ae784181d8a121cb3fbfe95c95efca5f4bd4
unknown <dsanjar@rocketmail.com>
2018-01-09 08:46:22 +0600

    body-parser,request modules added + minor updates

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const colors = require('colors');
 const open = require('open');
+const bodyParser = require('body-parser');
+const request = require('request');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const router = express.Router();
 
 const server = {
   host: 'localhost',
   port: 9000
 };
 
-app.get('/', (req, res) => {
-  res.send(`<h1>Hello world! It's ${ (new Date).getFullYear()} 🎄</h1>`);
-});
+app.use('/', router);
+
+router.route('/')
+  .get((req, res) => {
+    res.send(`<h1>Welcome Home 🏠! It's ${ new Date().getFullYear() } 🎄 </h1>`);
+  });
 
 app.listen(server.port, () => {
   open(`http://${server.host}:${server.port}`);
